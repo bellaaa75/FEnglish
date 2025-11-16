10a632dae1f1cf756158653cbd9be4ec2afeab10
Heeeenuooo <heyinuo@nuaa.edu.cn>
2025-11-17 00:38:12 +0800

    小修改

--- src/main/java/org/example/fenglish/repository/UserRepository.java
+++ src/main/java/org/example/fenglish/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package org.example.fenglish.repository;
 
 import org.example.fenglish.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
