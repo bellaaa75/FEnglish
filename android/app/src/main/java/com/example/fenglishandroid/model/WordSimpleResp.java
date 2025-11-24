1437d6bd4477f3562472b9fd3ac5f09af205319c
chenji <chenji.12138@nuaa.edu.cn>
2025-11-24 22:37:06 +0800

    增加了缺失的几个字段和方法

--- android/app/src/main/java/com/example/fenglishandroid/model/WordSimpleResp.java
+++ android/app/src/main/java/com/example/fenglishandroid/model/WordSimpleResp.java
@@ -5,6 +5,15 @@ public class WordSimpleResp {
     private String wordName;
     private String partOfSpeech;
     private String wordExplain;
+    private boolean isCollected;
+    public boolean isCollected() { return isCollected; }
+    public void setCollected(boolean collected) { isCollected = collected; }
+    /* ========== 新增字段 ========== */
+    private String thirdPersonSingular;
+    private String presentParticiple;
+    private String pastTense;
+    private String pastParticiple;
+    private String exampleSentence;
 
     // getter和setter
     public String getWordId() { return wordId; }
@@ -15,4 +24,40 @@ public class WordSimpleResp {
     public void setPartOfSpeech(String partOfSpeech) { this.partOfSpeech = partOfSpeech; }
     public String getWordExplain() { return wordExplain; }
     public void setWordExplain(String wordExplain) { this.wordExplain = wordExplain; }
+
+    /* ========== 新增 getter/setter ========== */
+    public String getThirdPersonSingular() {
+        return thirdPersonSingular;
+    }
+    public void setThirdPersonSingular(String thirdPersonSingular) {
+        this.thirdPersonSingular = thirdPersonSingular;
+    }
+
+    public String getPresentParticiple() {
+        return presentParticiple;
+    }
+    public void setPresentParticiple(String presentParticiple) {
+        this.presentParticiple = presentParticiple;
+    }
+
+    public String getPastTense() {
+        return pastTense;
+    }
+    public void setPastTense(String pastTense) {
+        this.pastTense = pastTense;
+    }
+
+    public String getPastParticiple() {
+        return pastParticiple;
+    }
+    public void setPastParticiple(String pastParticiple) {
+        this.pastParticiple = pastParticiple;
+    }
+
+    public String getExampleSentence() {
+        return exampleSentence;
+    }
+    public void setExampleSentence(String exampleSentence) {
+        this.exampleSentence = exampleSentence;
+    }
 }
